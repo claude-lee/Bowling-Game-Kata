233f942e31069feb50475a488e918ededdd3f3fc
Claudia Eisbrenner <Claudia.Eisbrenner@gmail.com>
2016-07-26 00:19:19 +0200

    3rd Failing TC

--- GameTest.java
+++ GameTest.java
@@ -1,12 +1,23 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 
 public class GameTest {
 	
 	@Test
-	public void canRoll(){
+	public void canRollBall(){
 		Game game = new Game();
 		game.roll(0);
+	}
+	
+	@Test
+	public void canScoreGutterGame(){
+		Game game = new Game();
+		for(int i=0; i<20; i++){
+			game.roll(0);
+		}
+		assertEquals(game.score(), 20);
 	}
 
 	
