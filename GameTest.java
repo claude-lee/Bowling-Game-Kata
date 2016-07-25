22ca95306b13c8194c647458da49b6e90297659a
Claudia Eisbrenner <Claudia.Eisbrenner@gmail.com>
2016-07-25 23:44:54 +0200

    Refactor: remove first TC

--- GameTest.java
+++ GameTest.java
@@ -4,11 +4,6 @@ import org.junit.Test;
 public class GameTest {
 	
 	@Test
-	public void canMakeGame(){
-		new Game();
-	}
-	
-	@Test
 	public void canRoll(){
 		Game game = new Game();
 		game.roll(0);
